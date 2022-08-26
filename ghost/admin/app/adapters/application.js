7f749389d18a8a87cb4ed16635dac11094d669b5
dothechuyen <dtn1430a0053@tuaf.edu.vn>
2022-08-26 12:28:59 +0700

    init

--- ghost/admin/app/adapters/application.js
+++
@@ -1,9 +0,0 @@
-import EmbeddedRelationAdapter from 'ghost-admin/adapters/embedded-relation-adapter';
-import classic from 'ember-classic-decorator';
-
-@classic
-export default class Application extends EmbeddedRelationAdapter {
-    shouldBackgroundReloadRecord() {
-        return false;
-    }
-}
